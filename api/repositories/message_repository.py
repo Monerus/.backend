a6b521dca845034eb214e8a674be5103c4b29ace
Maxim <monerus2020@gmail.com>
2026-01-24 20:13:32 +0300

    Initial commit of message_repository.py

--- api/repositories/message_repository.py
+++ api/repositories/message_repository.py
@@ -13,7 +13,7 @@ router = APIRouter(prefix="/messages", tags=["Messages"])
 @router.post('/', response_model=MessageResponse, status_code=status.HTTP_201_CREATED)
 async def create_message(
     message_in: MessageCreate,
-    current_messages: Message = Depends(get_current_user),
+    current_messages: Users = Depends(get_current_user),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     
     message = Message(**message_in.model_dump())
@@ -30,25 +30,20 @@ async def create_message(
 
 
 #Индивидуальные переписки
-@router.get("/{sender_id}/{recipient_id}")
-async def get_messages_users(sender_id: int, 
-                             recipient_id: int,
-                             current_user: dict = Depends(get_current_user),
+@router.get("/{recipient_id}/")
+async def get_messages_users(recipient_id: int,
+                             current_user: Users = Depends(get_current_user),
                              session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     
     current_user_id = current_user.id
-
-    
-    if current_user_id != sender_id and current_user_id != recipient_id:
-        raise HTTPException(status_code=403, detail="Доступ запрещён")
     
     stmt = select(Message).where(or_(
-        and_(Message.sender_id == sender_id, Message.recipient_id == recipient_id),
-        and_(Message.sender_id == recipient_id, Message.recipient_id == sender_id)
+        and_(Message.sender_id == current_user_id, Message.recipient_id == recipient_id),
+        and_(Message.sender_id == recipient_id, Message.recipient_id == current_user_id)
     ))
-
-    # Выполняем запрос
+    
     result = await session.execute(stmt)
     messages = result.scalars().all()
+    
     
     return messages 
